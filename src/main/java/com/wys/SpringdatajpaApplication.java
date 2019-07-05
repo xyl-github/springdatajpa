6f7ff6b83b016714782bca0ca26b2db277b34673
xyl-github <a1970382706@163.com>
2019-07-05 11:40:27 +0800

    修改

--- src/main/java/com/wys/SpringdatajpaApplication.java
+++ src/main/java/com/wys/SpringdatajpaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringdatajpaApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello");
         SpringApplication.run(SpringdatajpaApplication.class, args);
     }
 
